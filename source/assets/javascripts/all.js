dfd4d40ec1eb1aeb2b9398adf72fa3a4c5cd4cd3
Jon Lay <jon@hanno.co>
2015-12-05 19:46:24 +0800

    Clean up all the layout functions

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,15 +1,19 @@
+// Watch out for the equals sign in the "//=" at the beginning of lines.
+// This needs to be present for the scripts that you actually want to include
+
 //= require jquery/dist/jquery
 
 // TODO: PICKFRAMEWORK: Remove the one you don't want
-// Watch out for the //= at the beginning. This needs to be present for the
-// script that you actually want to include
 
 // ----- FOUNDATION
 //= require what-input/what-input.js
 //= require foundation-sites/dist/foundation.js
+$(document).foundation(); // this can be moved into app.js if you're definitely using Foundation
 
 // ----- BOOTSTRAP
 // require bootstrap-sass-official/assets/javascripts/bootstrap.js
 
+
+// ----- KICK OFF THE APP
 //= require app
 //= require script
--- source/assets/javascripts/app.js.coffee
+++ source/assets/javascripts/app.js.coffee
@@ -2,7 +2,4 @@
 # But you should really give coffeescript a go. You might also wanna check
 # out http://js2.coffee/ if you need to convert some javascript to coffeescript.
 
-# TODO: PICKFRAMEWORK: Remove this if you don't want Foundation
-$(document).foundation()
-
 # Other code goes below
